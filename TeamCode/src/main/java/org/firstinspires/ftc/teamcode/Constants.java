327866316eb2a2d098540b8d281ee9961e1eb2c0
Sophia5481 <sophiashenyongen@gmail.com>
2023-09-24 17:14:50 -0700

    added constants and vision stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -5,6 +5,20 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class Constants {
 
+    //Vision constants
+    public static int rLx = 5;
+    public static int rLy = 320;
+    public static int rLw = 100;
+    public static int rLh = 80;
 
+    public static int rRx = 500;
+    public static int rRy = 330;
+    public static int rRw = 135;
+    public static int rRh = 80;
+
+    public static int rMx = 140;
+    public static int rMy = 300;
+    public static int rMw = 300;
+    public static int rMh = 50;
 
 }
