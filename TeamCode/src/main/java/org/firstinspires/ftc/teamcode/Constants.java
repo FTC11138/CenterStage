c78ff751d00be662e180672721252d03415294a0
Nonify69 <98112485+Nonify69@users.noreply.github.com>
2023-09-27 17:45:28 -0700

    Added multiple fixed Pose Constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 
 @Config
 public class Constants {
@@ -11,7 +12,46 @@ public class Constants {
     public static double rotSpeed = 1;
 
 
+    public static double slowerMoveVel = 10;
+    public static double slowerSplineVel = 30;
 
+
+    /* -------------------------------------------- POSE CONSTANTS -------------------------------------------- */
+
+    // Start Poses
+    public static Pose2d redLeft_start = new Pose2d(-36, -63, Math.toRadians(90));
+    public static Pose2d redRight_start = new Pose2d(12, -63, Math.toRadians(90));
+    public static Pose2d blueLeft_start = new Pose2d(12, 63, Math.toRadians(-90));
+    public static Pose2d blueRight_start = new Pose2d(-36, 63, Math.toRadians(-90));
+
+
+    // Spike Mark Poses
+    public static Pose2d redLeft_right = new Pose2d(-24, -36, Math.toRadians(90));
+    public static Pose2d redLeft_center = new Pose2d(-36, -24, Math.toRadians(90));
+    public static Pose2d redLeft_left = new Pose2d(-48, -36, Math.toRadians(90));
+
+    public static Pose2d redRight_right = new Pose2d(24, -36, Math.toRadians(90));
+    public static Pose2d redRight_center = new Pose2d(36, -24, Math.toRadians(90));
+    public static Pose2d redRight_left = new Pose2d(48, -36, Math.toRadians(90));
+
+    public static Pose2d blueLeft_right = new Pose2d(24, 36, Math.toRadians(-90));
+    public static Pose2d blueLeft_center = new Pose2d(36, 24, Math.toRadians(-90));
+    public static Pose2d blueLeft_left = new Pose2d(48, 36, Math.toRadians(-90));
+
+    public static Pose2d blueRight_right = new Pose2d(-24, 36, Math.toRadians(-90));
+    public static Pose2d blueRight_center = new Pose2d(-36, 24, Math.toRadians(-90));
+    public static Pose2d blueRight_left = new Pose2d(-48, 36, Math.toRadians(-90));
+
+    // Backdrop poses
+    public static Pose2d redBackdrop = new Pose2d(48, -36, Math.toRadians(0));
+    public static Pose2d blueBackdrop = new Pose2d(48, 36, Math.toRadians(0));
+
+    // Park Poses
+    public static Pose2d redPark_right = new Pose2d(60, -60, Math.toRadians(0));
+    public static Pose2d redPark_left = new Pose2d(60, -12, Math.toRadians(0));
+
+    public static Pose2d bluePark_left = new Pose2d(60, 60, Math.toRadians(0));
+    public static Pose2d bluePark_right = new Pose2d(60, 12, Math.toRadians(0));
 
 
 
